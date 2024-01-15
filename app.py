184e2e81d47b70bc4bdd88028c0db3ade156e23a
mandanya <dan.zholk@gmail.com>
2024-01-15 05:06:47 +0300

    fix bar

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ while True:
 
         with fig_col2:
             st.markdown(f"<h4 style='text-align: center;'>Bars by {filter_period}s</h1>", unsafe_allow_html=True)
-            df_gb = df.groupby(pd.Grouper(key='Date', freq='D'))['Type'].value_counts()
+            df_gb = df.groupby(pd.Grouper(key='Date', freq='D'))['Type'].value_counts().reset_index()
             fig2 = px.bar(data_frame=df_gb, y='count', x='Date', color='Type')
             st.write(fig2)
 
